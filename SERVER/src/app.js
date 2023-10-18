15b5ac169256f48dcb529fe6f9a004f5447f3086
percy <percy.u.lomas@gmail.com>
2023-10-17 22:43:08 -0500

    server

---
+++ SERVER/src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cookieParser = require("cookie-parser");
+const morgan = require('morgan')
+
+const server =express();
+
+server.name = 'SERBER'
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cookieParser());
+server.use(morgan("dev"));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+server.get("/", (req, res) => {
+  
+    res.json({ info: "Pagina cargada con exito" });
+  });
+  server.use("/", routes);
+  
+  // Error catching endware.
+  server.use((err, req, res, next) => {
+    // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+  });
+
+  
+module.exports = server
---
+++ SERVER/src/db.js
@@ -0,0 +1,11 @@
+requier("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,{
+    logging: false, 
+    native: false,  mm
+    }      
+  );
