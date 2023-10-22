ff4b22ec21036456339547d38b5cfb40f12c1472
percy <percy.u.lomas@gmail.com>
2023-10-22 02:58:36 -0500

    styles-tailwind-config-and-navbar

--- client/src/componets/NavBar/Navbar.jsx
+++ client/src/componets/NavBar/Navbar.jsx
@@ -1,24 +1,25 @@
 import { Link } from 'react-router-dom';
 
+
 function Navbar() {
     let linkNav = [
-        {name: "Home", link: "/"},
+        {name: "Home", link: "/home"},
         {name: "Gallery", link: "/"},
         {name: "About", link: "/"},
     ];
 
     return (
-        <div>
-            <div>
-                <div>
-                    <span>
-                        hola
+        <div className='shadow-md w-full fixed top-0 left-0'>
+            <div className='md:flex items-center justify-between  bg-white py-4 md:px-10 px-7'>
+                <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
+                    <span className='text 3xl text-indigo-600 mr-1 pt-2'>
+                        Ecommerce
                     </span>
                 </div>
-                <ul>
+                <ul className='md:flex md:items-center'>
                     {linkNav.map((li) => (
-                        <li key={li.name}>
-                            <Link to={li.link}>{li.name}</Link>
+                        <li key={li.name} className='md:ml-8 text-xl'>
+                            <Link to={li.link} className='text-gray-800 hover:text-gray-400 duration-500'>{li.name}</Link>
                         </li>
                     ))}
                 </ul>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-//import { Link } from 'react-router-dom';
+
 
 function Home() {
   
     return (
-        <div>
-          <h1>hola</h1>
+        <div className='bg-blue-500'>
+          <h1>Home</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ut sed voluptatum saepe debitis culpa recusandae eos, vitae architecto expedita reiciendis excepturi quia quasi, porro possimus quam corporis repudiandae atque?</p>
         </div>
     );
 }
