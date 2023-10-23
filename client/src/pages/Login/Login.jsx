e116aed838b7e6465069eecb3248ddb89bc2a3f7
percy <percy.u.lomas@gmail.com>
2023-10-23 00:24:03 -0500

    fix-pages

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+  
+    return (
+        <div className='bg-gris w-full h-screen'>
+          <h1>Login</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ut sed voluptatum saepe debitis culpa recusandae eos, vitae architecto expedita reiciendis excepturi quia quasi, porro possimus quam corporis repudiandae atque?</p>
+        </div>
+    );
+}
+
+export default Login;
